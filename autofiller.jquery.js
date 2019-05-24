805bd52a0495ade366d09ccb89f513bbf97ba56d
Valentin Valev <biorkes@gmail.com>
2019-05-24 14:57:33 +0300

    Load after DOM

--- autofiller.jquery.js
+++ autofiller.jquery.js
@@ -1,29 +1,31 @@
-var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get('autofiller') == 1) {
+$(document).ready(function(){
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('autofiller') == 1) {
 
-    var fid, elem, pof;
+        var fid, elem, pof;
 
-    fid = urlParams.get('fid') !== null ? (urlParams.get('fid').length > 0 ? ($(`#${urlParams.get('fid')}`).length > 0 ? $(`#${urlParams.get('fid')} input`) : null) : null) : null;
-    pof = urlParams.get('pof') !== null ? (urlParams.get('pof').length > 0 ? ($(`input[name="${urlParams.get('pof')}"]`).length > 0 ? $(`input[name="${urlParams.get('pof')}"]`).parents('form').attr('id') : null) : null) : null;
-    ele = fid !== null ? fid : $(`#${pof} input`);
+        fid = urlParams.get('fid') !== null ? (urlParams.get('fid').length > 0 ? ($(`#${urlParams.get('fid')}`).length > 0 ? $(`#${urlParams.get('fid')} input`) : null) : null) : null;
+        pof = urlParams.get('pof') !== null ? (urlParams.get('pof').length > 0 ? ($(`input[name="${urlParams.get('pof')}"]`).length > 0 ? $(`input[name="${urlParams.get('pof')}"]`).parents('form').attr('id') : null) : null) : null;
+        ele = fid !== null ? fid : $(`#${pof} input`);
 
-    if (ele !== null) {
-        if (urlParams.get('af') !== null) {
-            ele.each(function() {
-                $(this).val(urlParams.get($(this).attr('name')));
-            });
-        } else {
-            if (urlParams.get('eid') !== null && $(`#${urlParams.get('eid')}`).length > 0) {
-                $(`#${urlParams.get('eid')}`).on('click', function() {
-                    ele.each(function() {
-                        $(this).val(urlParams.get($(this).attr('name')));
-                    })
+        if (ele !== null) {
+            if (urlParams.get('af') !== null) {
+                ele.each(function() {
+                    $(this).val(urlParams.get($(this).attr('name')));
                 });
             } else {
-                console.log('Erorr! No Event ID Click provided');
+                if (urlParams.get('eid') !== null && $(`#${urlParams.get('eid')}`).length > 0) {
+                    $(`#${urlParams.get('eid')}`).on('click', function() {
+                        ele.each(function() {
+                            $(this).val(urlParams.get($(this).attr('name')));
+                        })
+                    });
+                } else {
+                    console.log('Erorr! No Event ID Click provided');
+                }
             }
+        } else {
+            console.log('Invalid Selectors');
         }
-    } else {
-        console.log('Invalid Selectors');
     }
-}
+})
--- autofiller.js
+++ autofiller.js
@@ -1,29 +1,31 @@
-var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get('autofiller') == 1) {
+(function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('autofiller') == 1) {
 
-    var fid, elem, pof;
+        var fid, elem, pof;
 
-    fid = urlParams.get('fid') !== null ? (urlParams.get('fid').length > 0 ? (document.getElementById(`${urlParams.get('fid')}`).length > 0 ? document.querySelectorAll(`#${urlParams.get('fid')} input`) : null) : null) : null;
-    pof = urlParams.get('pof') !== null ? (urlParams.get('pof').length > 0 ? (document.getElementsByName(`${urlParams.get('pof')}`).length > 0 ? document.querySelector(`input[name="${urlParams.get('pof')}"]`).form : null) : null) : null;
-    ele = fid !== null ? fid : document.querySelectorAll(`#${pof.getAttribute("id")} input`);
+        fid = urlParams.get('fid') !== null ? (urlParams.get('fid').length > 0 ? (document.getElementById(`${urlParams.get('fid')}`).length > 0 ? document.querySelectorAll(`#${urlParams.get('fid')} input`) : null) : null) : null;
+        pof = urlParams.get('pof') !== null ? (urlParams.get('pof').length > 0 ? (document.getElementsByName(`${urlParams.get('pof')}`).length > 0 ? document.querySelector(`input[name="${urlParams.get('pof')}"]`).form : null) : null) : null;
+        ele = fid !== null ? fid : document.querySelectorAll(`#${pof.getAttribute("id")} input`);
 
-    if (ele !== null) {
-        if (urlParams.get('af') !== null) {
-            ele.forEach(function(element) {
-                element.value = urlParams.get(element.getAttribute("name"));
-            })
-        } else {
-            if (urlParams.get('eid') !== null && document.getElementById(`${urlParams.get('eid')}`).length > 0) {
-                document.getElementById(`${urlParams.get('eid')}`).onclick = function() {
-                    ele.forEach(function(element) {
-                        element.value = urlParams.get(element.getAttribute("name"));
-                    })
-                };
+        if (ele !== null) {
+            if (urlParams.get('af') !== null) {
+                ele.forEach(function(element) {
+                    element.value = urlParams.get(element.getAttribute("name"));
+                })
             } else {
-                console.log('Erorr! No Event ID Click provided');
+                if (urlParams.get('eid') !== null && document.getElementById(`${urlParams.get('eid')}`).length > 0) {
+                    document.getElementById(`${urlParams.get('eid')}`).onclick = function() {
+                        ele.forEach(function(element) {
+                            element.value = urlParams.get(element.getAttribute("name"));
+                        })
+                    };
+                } else {
+                    console.log('Erorr! No Event ID Click provided');
+                }
             }
+        } else {
+            console.log('Invalid Selectors');
         }
-    } else {
-        console.log('Invalid Selectors');
     }
 }
